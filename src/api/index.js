946a6ec9beaab1c4f7078f27204ae78e682779dd
Mohit <mohit.padhye2011@gmail.com>
2020-09-08 14:32:47 +0545

    card created

--- src/api/index.js
+++ src/api/index.js
@@ -5,9 +5,15 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async() => {
     try {
-        const respones = await axios.get(url);
-        return respones;
-        console.log(respones);
+        const {data:{confirmed,recovered,deaths,lastUpdate}} = await axios.get(url);
+         const modifiedData = {
+             confirmed,
+             recovered,
+             deaths,
+             lastUpdate,
+         }
+
+         return modifiedData;
     } catch (err) {
         
     }
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,8 +1,42 @@
 import React from 'react';
+import {Card,CardContent,Typography,Grid} from '@material-ui/core'
+import styles from './Cards.module.css'
 
-const Cards = () => {
+const Cards = (props) => {
+    console.log(props);
     return(
-        <h1>cards</h1>
+        <div className={styles.container}>
+                <Grid container spacing={3} justify="center">
+                       <Grid item component={Card}>
+                            <CardContent>
+                                <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                                <Typography varaint="h5" >Real Data</Typography>
+                                <Typography color="textSecondary" >Real Date</Typography>
+                                <Typography varaint="body2" >Number of active cases</Typography>
+                            </CardContent>
+                       </Grid>
+                       <Grid item component={Card}>
+                            <CardContent>
+                                <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                                <Typography varaint="h5" >Real Data</Typography>
+                                <Typography color="textSecondary" >Real Date</Typography>
+                                <Typography varaint="body2" >Number of recovires </Typography>
+                            </CardContent>
+                       </Grid>
+                       <Grid item component={Card}>
+                            <CardContent>
+                                <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                                <Typography varaint="h5" >Real Data</Typography>
+                                <Typography color="textSecondary" >Real Date</Typography>
+                                <Typography varaint="body2" >Number of deaths </Typography>
+                            </CardContent>
+                       </Grid>
+
+                </Grid>
+
+        </div>
+
+        
     )
 }
 export default Cards;
