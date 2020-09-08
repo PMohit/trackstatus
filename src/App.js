dde90cfc1049513a0100bc48408d16735f2943ad
Mohit <mohit.padhye2011@gmail.com>
2020-09-08 14:43:03 +0545

    data fetched from api

--- src/App.js
+++ src/App.js
@@ -5,17 +5,23 @@ import {fetchData} from './api';
 
 class App extends React.Component {
 
+  state = {
+    data:{},
+  }
+
  async componentDidMount(){
-   const data = await fetchData();
-   console.log(data);
+   const fetcheddata = await fetchData();
+  // console.log(fetcheddata);
+   this.setState({data:fetcheddata});
  }
 
 
 
   render() {
+    const {data} = this.state;
     return (
     <div className={styles.container}>
-       <Cards></Cards>       
+       <Cards data={data}></Cards>       
        <CountryPicker></CountryPicker>
        <Chart></Chart>
       
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,33 +1,56 @@
 import React from 'react';
 import {Card,CardContent,Typography,Grid} from '@material-ui/core'
-import styles from './Cards.module.css'
+import styles from './Cards.module.css';
+import CountUp from 'react-countup';
 
-const Cards = (props) => {
-    console.log(props);
+const Cards = ({data : {confirmed,recovered,deaths,lastUpdate}}) => {
+    //console.log(props);
+    if(!confirmed){
+        return 'Loading...'
+    }
     return(
         <div className={styles.container}>
                 <Grid container spacing={3} justify="center">
                        <Grid item component={Card}>
                             <CardContent>
                                 <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                                <Typography varaint="h5" >Real Data</Typography>
-                                <Typography color="textSecondary" >Real Date</Typography>
+                                <Typography varaint="h5" >
+                                     <CountUp start={0}
+                                     end={confirmed.value}
+                                     duration ={2.5}
+                                     separartor=","
+                                     />
+                                </Typography>
+                                
+                                <Typography color="textSecondary" >{new Date(lastUpdate).toDateString()}</Typography>
                                 <Typography varaint="body2" >Number of active cases</Typography>
                             </CardContent>
                        </Grid>
                        <Grid item component={Card}>
                             <CardContent>
                                 <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-                                <Typography varaint="h5" >Real Data</Typography>
-                                <Typography color="textSecondary" >Real Date</Typography>
+                                <Typography varaint="h5" >
+                                     <CountUp start={0}
+                                     end={recovered.value}
+                                     duration ={2.5}
+                                     separartor=","
+                                     />
+                                </Typography>
+                                <Typography color="textSecondary" >{new Date(lastUpdate).toDateString()}</Typography>
                                 <Typography varaint="body2" >Number of recovires </Typography>
                             </CardContent>
                        </Grid>
                        <Grid item component={Card}>
                             <CardContent>
                                 <Typography color="textSecondary" gutterBottom>Deaths</Typography>
-                                <Typography varaint="h5" >Real Data</Typography>
-                                <Typography color="textSecondary" >Real Date</Typography>
+                                <Typography varaint="h5" >
+                                     <CountUp start={0}
+                                     end={deaths.value}
+                                     duration ={2.5}
+                                     separartor=","
+                                     />
+                                </Typography>
+                                <Typography color="textSecondary" >{new Date(lastUpdate).toDateString()}</Typography>
                                 <Typography varaint="body2" >Number of deaths </Typography>
                             </CardContent>
                        </Grid>
